4cdd15fe9bea8d252d52789a8e3ad44b97af365a
rushinpresence18-cloud <rushinpresence18@gmail.com>
2026-05-22 15:34:06 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// Create an array of colors
+let colors = []
+colors.push("Green", "Blue", "Red")
+
+// Array of Numbers
+let numbers = ["3", "6", "18", "20"]
+
+// Removing the last color of the Array
+let updatedcolors = colors.filter(color => color !== "Red");
+console.log(updatedcolors);
+
+// Adding a New color to the Array
+let colors = ["Green", "Blue", "Red"]
+colors.push ("Silver");
+console.log (colors);
+
+let colors = ["Green", "Blue", "Red"];
+let numbers = ["3", "6", "18", "20"]
+ 
+console.log (colors);
+console.log (numbers);
+
+
+
+
+
